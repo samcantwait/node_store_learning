5b883c76e063362e192e21dfb97388bafe9496ed
Samantha Atkins <sam@samcantwait.com>
2023-05-13 16:05:19 -0500

    add pug and handlebars templates

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const path = require('path');
+const { engine } = require('express-handlebars');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.engine('hbs', engine({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).render('404', { pageTitle: 'Page not Found' })
 })
 
 const PORT = 3000;
--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path')
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    const products = adminData.products;
+    res.render('shop.hbs', { pageTitle: 'Shop', prod: products, path: '/', hasProd: products.length > 0, activeShop: true});
 });
 
 module.exports = router;
