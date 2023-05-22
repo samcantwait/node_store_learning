b0360bf89d67d8a60c8694ffaaa10cfb3f30d6c4
Samantha Atkins <samatkins@gmail.com>
2023-05-22 17:44:29 -0500

    add fields to product input page

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,6 +9,10 @@ exports.getAddProduct = (req, res, next) => {
 }
 
 exports.postAddProduct = (req, res, next) => {
+    const title = req.body.title;
+    const image = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
     const product = new Product(req.body.title);
     product.save();
     res.redirect('/');
