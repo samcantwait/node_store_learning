86e953641d832bd2262a9ea5e1670b225807cf5a
Samantha Atkins <samatkins@gmail.com>
2023-07-15 21:42:21 -0500

    add signup page and incorporate password hashing using bcryptjs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
-const User = require('../models/user')
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
@@ -8,26 +9,57 @@ exports.getLogin = (req, res, next) => {
     })
 }
 
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        isAuthenticated: false
+    });
+};
+
 exports.postLogin = (req, res, next) => {
     const { email, password } = req.body;
     User.findOne({ email: email })
         .then(user => {
+            console.log('user is: ', user)
             if (user) {
                 req.session.user = user;
                 req.session.isLoggedIn = true;
                 req.session.save(err => {
                     res.redirect('/');
                 })
-            }
-            res.redirect('/');
+            } else res.redirect('/');
         })
         .catch(err => {
             console.log(err)
         })
 }
 
+exports.postSignup = (req, res, next) => {
+    const { email, password, confirmPassword } = req.body;
+    User.findOne({ email: email })
+        .then(userDoc => {
+            if (userDoc) {
+                return res.redirect('/signup');
+            }
+            return bcrypt.hash(password, 12);
+        })
+        .then(hashedPass => {
+            const user = new User({
+                email,
+                password: hashedPass,
+                cart: { items: [] }
+            })
+            return user.save();
+        })
+        .then(() => {
+            res.redirect('/login')
+        })
+        .catch(err => console.log(err))
+}
+
 exports.postLogout = (req, res, next) => {
-    req.session.destroy(err => {
+    req.session.destroy(() => {
         res.redirect('/');
     })
 }
