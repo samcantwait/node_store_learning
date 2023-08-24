782f3d671a5179012b317ab9bafb517baf66b91a
Samantha Atkins <samatkins@gmail.com>
2023-08-24 15:10:38 -0500

    begin implementing web safety features

--- app.js
+++ app.js
@@ -1,105 +1,147 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const csrf = require('csurf');
-const flash = require('connect-flash');
-const multer = require('multer');
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const multer = require("multer");
+// helmet is used for setting secure response headers
+const helmet = require("helmet");
+// compression is used to compress files for better user experience
+const compression = require("compression");
+// morgan is uset for request logging and we will save logs in access.log
+const morgan = require("morgan");
+require("dotenv").config();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
-const errorController = require('./controllers/errors');
-const User = require('./models/user');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+const errorController = require("./controllers/errors");
+const User = require("./models/user");
 
-const MONGODB_URI = `mongodb+srv://samatkins:${process.env.MONGODB_PASS}@nodecluster.s7zhua5.mongodb.net/shop?&w=majority`;
+const MONGODB_URI = `mongodb+srv://samatkins:${process.env.MONGODB_PASS}@nodecluster.s7zhua5.mongodb.net/shop?retryWrites=true&w=majority`;
 
 const app = express();
 const store = new MongoDBStore({
-    uri: MONGODB_URI,
-    collection: 'sessions'
+  uri: MONGODB_URI,
+  collection: "sessions",
 });
 // We are using this package to prevent csrf attacks. It sends a token to our server to identify our site before a user can make a post request.
 const csrfProtection = csrf();
 
 const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'images');
-    },
-    filename: (req, file, cb) => {
-        cb(null, new Date().toISOString().replace(/:/g, '-') + '-' + file.originalname)
-    }
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
+    );
+  },
 });
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-}
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// set up the file for storing logs
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        "script-src": ["'self'"],
+        "img-src": ["'self'", "https: data:"],
+      },
+    },
+  })
+);
+app.use(compression());
+app.use(morgan("combined", { stream: accessLogStream }));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
-    session({
-        secret: process.env.SECRET,
-        resave: false,
-        saveUninitialized: false,
-        store: store
-    })
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
 );
 app.use(csrfProtection);
 app.use(flash());
 
+// use this route along with the command: curl http://localhost:3000/test/ --include
+// this will show headers. Environment must be running.
+app.get("/test", (req, res, next) => {
+  res.json("hello world");
+});
+
 app.use((req, res, next) => {
-    if (!req.session.user) {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      if (!user) {
         return next();
-    }
-    User.findById(req.session.user._id)
-        .then(user => {
-            if (!user) {
-                return next();
-            }
-            req.user = user;
-            next();
-        })
-        .catch(err => {
-            throw new Error(err)
-        });
+      }
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
 });
 
 // Expressjs has locals. These are only passed into the views which are rendered.
 app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
-})
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.get('/500', errorController.get500);
+app.get("/500", errorController.get500);
 
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-    console.log(error)
-    res.redirect('/500');
-})
+  console.log(error);
+  res.redirect("/500");
+});
 
 mongoose
-    .connect(MONGODB_URI)
-    .then(() => {
-        const PORT = 3000;
-        app.listen(PORT, () => {
-            console.log(`Connected on port: ${PORT}`);
-        });
-    })
-    .catch(err => console.log(err));
+  .connect(MONGODB_URI)
+  .then(() => {
+    const PORT = 3000;
+    app.listen(PORT, () => {
+      console.log(`Connected on port: ${PORT}`);
+      console.log(process.env.NODE_ENV);
+    });
+  })
+  .catch((err) => console.log(err));
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,234 +1,234 @@
-const Product = require('../models/product');
-const Order = require('../models/order');
-const stripe = require('stripe')(process.env.STRIPE_KEY)
+const Product = require("../models/product");
+const Order = require("../models/order");
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const PDFDocument = require('pdfkit');
+const PDFDocument = require("pdfkit");
 
 const ITEMS_PER_PAGE = 2;
 
 exports.getProducts = (req, res, next) => {
-    const page = +req.query.page || 1;
-    let totalItems;
-    Product
-        .find()
-        .countDocuments()
-        .then(numProducts => {
-            totalItems = numProducts;
-            return Product.find()
-                .skip((page - 1) * ITEMS_PER_PAGE)
-                .limit(ITEMS_PER_PAGE)
-        })
-        .then(products => {
-            res.render('shop/product-list', {
-                pageTitle: 'Products',
-                prod: products,
-                path: '/products',
-                currentPage: page,
-                totalProducts: totalItems,
-                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-                hasPreviousPage: page > 1,
-                nextPage: page + 1,
-                previousPage: page - 1,
-                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
-            });
-        });
-}
+  const page = +req.query.page || 1;
+  let totalItems;
+  Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
+    .then((products) => {
+      res.render("shop/product-list", {
+        pageTitle: "Products",
+        prod: products,
+        path: "/products",
+        currentPage: page,
+        totalProducts: totalItems,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
+      });
+    });
+};
 
 exports.getProduct = (req, res, next) => {
-    const prodId = req.params.productId;
-    Product.findById(prodId)
-        .then(product => {
-            res.render('shop/product-detail',
-                {
-                    product,
-                    pageTitle: product.title,
-                    path: '/products',
-
-                })
-        })
-}
+  const prodId = req.params.productId;
+  Product.findById(prodId).then((product) => {
+    res.render("shop/product-detail", {
+      product,
+      pageTitle: product.title,
+      path: "/products",
+    });
+  });
+};
 
 exports.getIndex = (req, res, next) => {
-    const page = +req.query.page || 1;
-    let totalItems;
-    Product
-        .find()
-        .countDocuments()
-        .then(numProducts => {
-            totalItems = numProducts;
-            return Product.find()
-                .skip((page - 1) * ITEMS_PER_PAGE)
-                .limit(ITEMS_PER_PAGE)
-        })
-        .then(products => {
-            res.render('shop/index', {
-                pageTitle: 'Shop',
-                prod: products,
-                path: '/',
-                currentPage: page,
-                totalProducts: totalItems,
-                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-                hasPreviousPage: page > 1,
-                nextPage: page + 1,
-                previousPage: page - 1,
-                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
-            });
-        });
-}
+  const page = +req.query.page || 1;
+  let totalItems;
+  Product.find()
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
+    .then((products) => {
+      res.render("shop/index", {
+        pageTitle: "Shop",
+        prod: products,
+        path: "/",
+        currentPage: page,
+        totalProducts: totalItems,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
+      });
+    });
+};
 
 exports.getCart = (req, res, next) => {
-    req.user
-        .populate('cart.items.productId')
-        .then(user => {
-            res.render('shop/cart', {
-                pageTitle: 'Your Cart',
-                products: user.cart.items,
-                path: '/cart',
-            })
-        })
-}
+  req.user.populate("cart.items.productId").then((user) => {
+    res.render("shop/cart", {
+      pageTitle: "Your Cart",
+      products: user.cart.items,
+      path: "/cart",
+    });
+  });
+};
 
 exports.postCart = (req, res, next) => {
-    const { productId } = req.body;
-    Product.findById(productId)
-        .then(product => {
-            return req.user.addToCart(product);
-        })
-        .then(result => res.redirect('/cart'))
-        .catch(err => console.log(err));
-}
+  const { productId } = req.body;
+  Product.findById(productId)
+    .then((product) => {
+      return req.user.addToCart(product);
+    })
+    .then((result) => res.redirect("/cart"))
+    .catch((err) => console.log(err));
+};
 
 exports.postCartDeleteProduct = (req, res, next) => {
-    const { productId } = req.body;
-    req.user
-        .removeFromCart(productId)
-        .then(result => res.redirect('/cart'))
-        .catch(err => console.log(err));
-}
+  const { productId } = req.body;
+  req.user
+    .removeFromCart(productId)
+    .then((result) => res.redirect("/cart"))
+    .catch((err) => console.log(err));
+};
 
 exports.getCheckout = (req, res, next) => {
-    let products;
-    let total;
-    req.user
-        .populate('cart.items.productId')
-        .then(user => {
-            products = user.cart.items;
-            total = 0;
-            products.forEach(p => {
-                total += p.quantity * p.productId.price;
-            })
+  let products;
+  let total;
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      products = user.cart.items;
+      total = 0;
+      products.forEach((p) => {
+        total += p.quantity * p.productId.price;
+      });
 
-            return stripe.checkout.sessions.create({
-                payment_method_types: ['card'],
-                line_items: products.map(p => {
-                    return {
-                        price_data: {
-                            currency: 'USD',
-                            product_data: {
-                                name: p.productId.title,
-                                description: p.productId.description,
-                            },
-                            unit_amount: Math.floor(p.productId.price * 100),
-                        },
-                        quantity: p.quantity
-                    };
-                }),
-                mode: 'payment',
-                success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
-                cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel',
-            });
-        })
-        .then(session => {
-            res.render('shop/checkout', {
-                path: '/checkout',
-                pageTitle: 'Checkout',
-                products: products,
-                totalSum: total,
-                sessionId: session.id
-            })
-        })
-        .catch(err => next(err))
-}
+      return stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: products.map((p) => {
+          return {
+            price_data: {
+              currency: "USD",
+              product_data: {
+                name: p.productId.title,
+                description: p.productId.description,
+              },
+              unit_amount: Math.floor(p.productId.price * 100),
+            },
+            quantity: p.quantity,
+          };
+        }),
+        mode: "payment",
+        success_url:
+          req.protocol + "://" + req.get("host") + "/checkout/success",
+        cancel_url: req.protocol + "://" + req.get("host") + "/checkout/cancel",
+      });
+    })
+    .then((session) => {
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum: total,
+        sessionId: session.id,
+      });
+    })
+    .catch((err) => next(err));
+};
 
 exports.getCheckoutSuccess = (req, res, next) => {
-    req.user
-        .populate('cart.items.productId')
-        .then(user => {
-            const products = user.cart.items.map(i => {
-                return { quantity: i.quantity, productData: { ...i.productId._doc } }
-            });
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, productData: { ...i.productId._doc } };
+      });
 
-            const order = new Order({
-                user: {
-                    userId: req.user,
-                    email: req.user.email
-                },
-                products: products
-            });
-            return order.save();
-        })
-        .then(() => {
-            return req.user.clearCart()
-        })
-        .then(() => {
-            res.redirect('/orders');
-        })
-        .catch(err => console.log(err))
-}
+      const order = new Order({
+        user: {
+          userId: req.user,
+          email: req.user.email,
+        },
+        products: products,
+      });
+      return order.save();
+    })
+    .then(() => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect("/orders");
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.getOrders = (req, res, next) => {
-    Order.find({ 'user.userId': req.user._id })
-        .then(orders => {
-            res.render('shop/orders', {
-                path: '/orders',
-                pageTitle: 'Your Orders',
-                orders,
-            });
-        })
-}
+  Order.find({ "user.userId": req.user._id }).then((orders) => {
+    res.render("shop/orders", {
+      path: "/orders",
+      pageTitle: "Your Orders",
+      orders,
+    });
+  });
+};
 
 exports.getInvoice = (req, res, next) => {
-    const { orderId } = req.params;
-    Order.findById(orderId)
-        .then(order => {
-            if (!order) {
-                return next(new Error('No order found.'))
-            };
-            if (order.user.userId.toString() !== req.user._id.toString()) {
-                return next(new Error('Not authorized.'))
-            };
+  const { orderId } = req.params;
+  Order.findById(orderId)
+    .then((order) => {
+      if (!order) {
+        return next(new Error("No order found."));
+      }
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error("Not authorized."));
+      }
 
-            const invoiceName = `invoice-${orderId}.pdf`;
-            const invoicePath = path.join('data', 'invoices', invoiceName);
+      const invoiceName = `invoice-${orderId}.pdf`;
+      const invoicePath = path.join("data", "invoices", invoiceName);
 
-            // Create a PDF on the fly
-            const pdfDoc = new PDFDocument();
-            pdfDoc.pipe(fs.createWriteStream(invoicePath));
-            pdfDoc.pipe(res);
+      // Create a PDF on the fly
+      const pdfDoc = new PDFDocument();
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
 
-            pdfDoc.fontSize(26).text('Invoice', {
-                underline: true
-            });
-            pdfDoc.text('------------------------------');
-            let totalPrice = 0;
-            order.products.forEach(prod => {
-                totalPrice += prod.quantity * prod.productData.price;
-                pdfDoc.fontSize(14).text(prod.productData.title + ' - ' + prod.quantity + ' x $' + prod.productData.price);
-            });
-            pdfDoc.text('------');
-            pdfDoc.fontSize(18).text('Total Price: $' + totalPrice);
+      pdfDoc.fontSize(26).text("Invoice", {
+        underline: true,
+      });
+      pdfDoc.text("------------------------------");
+      let totalPrice = 0;
+      order.products.forEach((prod) => {
+        totalPrice += prod.quantity * prod.productData.price;
+        pdfDoc
+          .fontSize(14)
+          .text(
+            prod.productData.title +
+              " - " +
+              prod.quantity +
+              " x $" +
+              prod.productData.price
+          );
+      });
+      pdfDoc.text("------");
+      pdfDoc.fontSize(18).text("Total Price: $" + totalPrice);
 
-            pdfDoc.end();
+      pdfDoc.end();
 
-
-            // Read a PDF that was already created.
-            // const file = fs.createReadStream(invoicePath);
-            // res.setHeader('Content-Type', 'application/pdf');
-            // res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
-            // file.pipe(res);
-        })
-        .catch(err => next(err));
-}
+      // Read a PDF that was already created.
+      // const file = fs.createReadStream(invoicePath);
+      // res.setHeader('Content-Type', 'application/pdf');
+      // res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
+      // file.pipe(res);
+    })
+    .catch((err) => next(err));
+};
